f983c9ce1e93e0fb1a1f13ccbc561a8b41af20de
javiburn <jsarabia98@gmail.com>
2023-06-21 19:05:07 +0200

    WIP on improving the parsing of redirections

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: jsarabia <jsarabia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 12:21:13 by alaparic          #+#    #+#             */
-/*   Updated: 2023/06/21 17:18:48 by jsarabia         ###   ########.fr       */
+/*   Updated: 2023/06/21 19:00:09 by jsarabia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,44 @@
  * This function separates the raw user input onto a list where each element is
  * a command
 */
+
+char	**order_values(char	**old_arr)
+{
+	t_vars	new;
+
+	new.i = 0;
+	new.j = 0;
+	while (old_arr[new.j])
+		new.j++;
+	new.arr = ft_calloc(new.j + 1, sizeof(char *));
+	new.j = 0;
+	while (old_arr[new.j])
+	{
+		if (old_arr[new.j][0] != '<' && old_arr[new.j][0] != '>' && \
+	(new.j < 1 || (old_arr[new.j - 1][0] != '<'
+			&& old_arr[new.j - 1][0] != '>')))
+		{
+			printf("JAJA, %s arr[%d][0] = %c\n", old_arr[new.j], new.j, old_arr[new.j][0]);
+			new.arr[new.i] = ft_substr(old_arr[new.j], 0,
+					ft_strlen(old_arr[new.j]));
+			new.i++;
+		}
+		new.j++;
+	}
+	new.j = 0;
+	while (old_arr[new.j])
+	{
+		if (old_arr[new.j][0] == '<' || old_arr[new.j][0] == '>')
+		{
+			new.arr[new.i] = ft_substr(old_arr[new.j], 0, ft_strlen(old_arr[new.j]));
+			new.i++;
+		}
+		new.j++;
+	}
+	free_matrix(old_arr);
+	return (new.arr);
+}
+
 t_list	*split_commands(char *input, t_list *com)
 {
 	char			*str;
@@ -28,8 +66,7 @@ t_list	*split_commands(char *input, t_list *com)
 	old_pos = 0;
 	while (input[i])
 	{
-		if (check_flag(input, i) == NONE && i > 0 && (input[i] == 124 \
-		|| ((input[i] == 62 || input[i] == 60) && input[i - 1] != input[i])))
+		if (check_flag(input, i) == NONE && input[i] == 124)
 		{
 			temp = ft_substr(str, old_pos, i - old_pos);
 			if (com == NULL)
@@ -61,6 +98,7 @@ void	parsing(char *input, char **paths, char **env)
 	while (aux)
 	{
 		aux->content = split_words(aux->content);
+		aux->content = order_values(aux->content);
 		aux->content = expand_values(aux->content, env);
 		int i = 0;
 		printf("\033[0;35mParsed:\033[0m\n");
